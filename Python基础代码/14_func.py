ce4b157212f90503b01edb7f72987789b8ca1e17
芦苇 <47054673+luwei0915@users.noreply.github.com>
2019-11-08 17:21:59 +0800

    Create 14_func.py

---
+++ Python基础代码/14_func.py
@@ -0,0 +1,9 @@
+def init(name,gender,age=6,city='beijing'):
+    print('name',name)
+    print('gender',gender)
+    print('age',age)
+    print('city',city)
+'''调用函数'''
+init('zs','boy')
+print()
+init('ls','girl',8)
