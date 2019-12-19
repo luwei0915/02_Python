a7c700db0fc37659af76b0aa3393e2b1954513d2
芦苇 <47054673+luwei0915@users.noreply.github.com>
2019-12-19 14:59:16 +0800

    Create 05_斐波那契数列.py

---
+++ python100例/05_斐波那契数列.py
@@ -0,0 +1,12 @@
+'''
+题目：斐波那契数列
+程序分析：斐波那契数列（Fibonacci sequence），
+又称黄金分割数列，指的是这样一个数列：
+0、1、1、2、3、5、8、13、21、34、……
+'''
+
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n-1) + fib(n-2)
+print(fib(3))
