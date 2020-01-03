3003f3e27167dad5210ee1050e15fb93ee531242
芦苇 <47054673+luwei0915@users.noreply.github.com>
2020-01-03 13:18:27 +0800

    Create 11_multiprocessing_6.py

---
+++ Python100模块/11_multiprocessing_6.py
@@ -0,0 +1,24 @@
+# 进程间状态信息
+import multiprocessing
+import time
+
+def wait_for_event(e):
+    print("wait for event:starting")
+    e.wait()
+    print("wait for event:e_is_set()->", e.is_set())
+
+def wait_for_event_timeout(e, t):
+    print("wait_for_event_timeout:starting")
+    e.wait(t)
+    print("wait_for_event_timeout:e_is_set()->", e.is_set())
+
+if __name__ == '__main__':
+    e = multiprocessing.Event()
+    w1 = multiprocessing.Process(name="block",target=wait_for_event,args=(e,))
+    w1.start()
+    w2 = multiprocessing.Process(name="nonblock", target=wait_for_event_timeout,args=(e, 2))
+    w2.start()
+    print("main:waiting before calling Event.set()")
+    time.sleep(3)
+    e.set()
+    print("main:event is set")
